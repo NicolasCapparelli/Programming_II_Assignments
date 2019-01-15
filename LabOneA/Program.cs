6caa207860f16cb3b9006a828342bdd48472168c
NicoCappa <capparellinicog@hotmail.com>
2019-01-15 06:48:00 -0500

    LabOneA and LabOneB

---
+++ LabOneA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LabOneA {
+    internal class Program {
+        public static void Main(string[] args) {
+
+            int duration;
+            int frequency;
+
+            do {
+                
+                Console.WriteLine("Enter Frequency (37 to 32767):");
+                frequency = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter Duration:");
+                duration = Convert.ToInt32(Console.ReadLine());
+                
+            } while (frequency < 38 || frequency > 32767);           
+            Console.Beep(frequency, duration);
+        }
+    }
+}
---
+++ LabOneB/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace LabOneB {
+    internal class Program {
+        public static void Main(string[] args) {
+            
+            // Part one
+            int[] nums = {1, 4, 13, 43, -25, 17, 22, -37, 29};
+            var largestNum = FindLargest(nums);
+            Console.WriteLine("Largest value in nums: " + largestNum);
+            
+            // Part two
+            var data = new int[20];
+            FillArray(data);
+            var largestData = FindLargest(data);
+            Console.WriteLine("Largest value in data: " + largestData);
+            
+            // Part Three
+            Console.WriteLine("Sum of largest: " + (largestNum + largestData));
+            Console.WriteLine("[{0}]", string.Join(", ", data));
+            
+            // Part Four
+            Console.WriteLine(LongestPositiveStreak(new []{4,5,0,2,-1,88,78,66,-6}));
+
+        }
+
+        private static int FindLargest(int[] searchMe) {
+            return searchMe.Max();
+        }
+
+        private static int[] FillArray(int[] fillMe) {
+            var random = new Random();
+            
+            for (var i = 0; i < fillMe.Length; i++) {
+                fillMe[i] = random.Next(-100, 100);
+            }
+
+            return fillMe;
+        }
+
+        private static int LongestPositiveStreak(int[] streakArray) {
+
+            var longestStreak = 0;
+            var streak = 0;
+
+            foreach (var num in streakArray) {
+                if (num > 0) {
+                    streak++;
+                }
+                else {
+                    longestStreak = streak;
+                    streak = 0;
+                }
+            }
+
+            return streak > longestStreak ? streak : longestStreak;
+        }
+    }
+}
