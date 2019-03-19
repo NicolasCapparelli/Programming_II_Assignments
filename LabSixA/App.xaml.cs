ff2362e814891366706f7467f14a30186989ccfd
NicoCappa <capparellinicog@hotmail.com>
2019-03-19 07:11:22 -0400

    - LabSixA Halfway

---
+++ LabSixA/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace LabSixA {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+    }
+}
---
+++ LabSixA/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * Class: CSE 1322L
+ * Section: 07
+ * Term: Spring
+ * Instructor: Kevin Markley
+ * Name: Nicolas Capparelli
+ * Lab#: 5B
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace LabSixA {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        
+        private void OpenFile(object sender, RoutedEventArgs e) {
+
+        }
+        
+        private void SaveFile(object sender, RoutedEventArgs e) {
+            
+            var dialog = new SaveFileDialog(); 
+            dialog.FileName = "MyFile.txt";
+            dialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            
+            // Show dialog
+            if (dialog.ShowDialog() ?? false) {
+                
+                // Create file
+                using (var sw = new StreamWriter(dialog.FileName)) {
+                    
+                    // Split text into lines
+                    var list = new List<string>(TextBox.Text.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+                    
+                    // Write each line in file
+                    foreach (var line in list) {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
